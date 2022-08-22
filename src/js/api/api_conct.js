4251ddc00a1d93bc8c824892542bfa90421644a6
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-08-22 16:18:58 +0300

    reboot

---
+++ src/js/api/api_conct.js
@@ -0,0 +1,7 @@
+const API_KEY = '8760b701d30b729e44d512f24647f496';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const TREND_URL = 'https://developers.themoviedb.org/3/trending/get-trending';
+const FIND_FILM = 'https://developers.themoviedb.org/3/search/search-movies';
+const FULL_INFO_FILM = 'https://developers.themoviedb.org/3/movies/get-movie-details';
+
+export { API_KEY, BASE_URL, TREND_URL, FIND_FILM, FULL_INFO_FILM };
